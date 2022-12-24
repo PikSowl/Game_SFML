295529bc37d82bb43968973826373e965fbd5bbd
PikSowl <kmazalov11@gmail.com>
2022-12-24 12:02:25 +0200

    Added basics

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Func.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+
+    return 0;
+}
---
+++ src/Func.cpp
@@ -0,0 +1,12 @@
+#include <Func.h>
+#include <iostream>
+
+using std::swap;
+using std::cin;
+using std::cout;
+using std::endl;
+
+namespace ps
+{
+
+}
